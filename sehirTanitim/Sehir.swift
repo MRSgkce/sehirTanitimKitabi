f86621683003b3e7ca9d16ed85faf928065b70f2
MRSgkce <mursidegokce985@gmail.com>
2024-08-16 16:14:44 +0300

    Sehir sınıfı yani modeli oluşturuldu. Diğer sınıflarda bu modeli kullanarak bir şablon gibi kullanıldı. init yaparak ilk tanımlandığında varsayılan değer vermeye gerek olmadı.

--- sehirTanitim/Sehir.swift
+++ sehirTanitim/Sehir.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import UIKit
+
+class Sehir{
+    var isim : String
+    var bolge : String
+    var gorsel : UIImage
+    
+    init(isim: String, bolge: String, gorsel: UIImage) {
+        self.isim = isim
+        self.bolge = bolge
+        self.gorsel = gorsel
+    }
+    
+}
